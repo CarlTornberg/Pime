8641e9895c5d47fda8726c92d6381dc5fa3a92c0
Carl Tornberg <carltornberg@hotmail.com>
2025-12-09 07:27:15 +0100

    Added withdraw from vault processor structure

---
+++ src/processors/vault/withdraw_from_vault.rs
@@ -0,0 +1,6 @@
+use pinocchio::{ProgramResult, account_info::AccountInfo};
+
+pub fn process_withdraw_from_vault(accounts: &[AccountInfo], instrution_data: &[u8]) -> ProgramResult {
+    
+    ProgramResult::Ok(())
+}
