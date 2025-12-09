efbc5d966fb59f2c9f0110d0f1762ec3a192a21a
Carl Tornberg <carltornberg@hotmail.com>
2025-12-09 13:37:45 +0100

    Added helper function to create new mint and mint to ATA

--- tests/litesvm.rs
+++ tests/litesvm.rs
@@ -1,40 +1,37 @@
 #[cfg(test)]
 mod litesvm_tests {
     use litesvm::LiteSVM;
+    use solana_sdk::program_error::ProgramError;
     use solana_sdk::{message::Message, native_token::LAMPORTS_PER_SOL, program_pack::Pack, pubkey::Pubkey, rent, signature::Keypair, signer::Signer, transaction::Transaction};
-    use spl_token_interface::state::Mint;
+    use spl_token_interface::state::{GenericTokenAccount, Mint};
+    use spl_token_interface::state::Account as TokenAccount;
 
     #[test]
     fn create_vault() {
         let mut svm = LiteSVM::new();
 
         let pime_id = Pubkey::new_from_array(pime::ID);
+        let token_program = spl_token_interface::ID;
+        // let native_mint = spl_token_interface::native_mint::ID;
 
         let from_keypair = Keypair::new();
         let from = from_keypair.pubkey();
         let to = Pubkey::new_unique();
+        
+        svm.airdrop(&from, LAMPORTS_PER_SOL).unwrap();
 
-        let token_program = spl_token_interface::ID;
-        let native_mint = spl_token_interface::native_mint::ID;
+        create_and_mint_to(&mut svm, &from_keypair, &to, 100_000, &token_program).unwrap();
+
+    }
+
+    // Helpers 
+    
+    /// Creates a new mint, and mints the token to the to account.
+    /// This function initializes the ATA of the to account.
+    ///
+    /// Returns the new mint address.
+    fn create_and_mint_to(svm: &mut LiteSVM, mint_authority: &Keypair, to: &Pubkey, mint_amount: u64, token_program: &Pubkey) -> Result<Pubkey, ProgramError> {
         let mint = Keypair::new();
-
-        let mint_space = Mint::LEN;
-        let create_mint_account_inst = solana_system_interface::instruction::create_account(
-            /* from */ &from, 
-            /* to */ &mint.pubkey(),
-            /* lamports */ svm.minimum_balance_for_rent_exemption(mint_space),
-            /* space */ mint_space as u64,
-            /* owner */ &token_program,
-        );
-
-        let init_mint_inst = spl_token_interface::instruction::initialize_mint(
-            /* token program */ &token_program, 
-            /* mint */ &mint.pubkey(), 
-            /* mint authority */ &from, 
-            /* freeze authority */ None, 
-            /* decimals */ 6,
-        ).unwrap();
-
         let to_ata = Pubkey::find_program_address(
             &[
                 &to.to_bytes(),
@@ -43,25 +40,39 @@ mod litesvm_tests {
             ], 
             &spl_associated_token_account_interface::program::ID);
 
+        let create_mint_account_inst = solana_system_interface::instruction::create_account(
+            /* from */ &mint_authority.pubkey(), 
+            /* to */ &mint.pubkey(),
+            /* lamports */ svm.minimum_balance_for_rent_exemption(Mint::LEN),
+            /* space */ Mint::LEN as u64,
+            /* owner */ token_program,
+        );
+
+        let init_mint_inst = spl_token_interface::instruction::initialize_mint(
+            /* token program */ token_program, 
+            /* mint */ &mint.pubkey(), 
+            /* mint authority */ &mint_authority.pubkey(), 
+            /* freeze authority */ None, 
+            /* decimals */ 6,
+        )?;
+
         let create_ata_inst = spl_associated_token_account_interface::instruction::create_associated_token_account(
-            /* funding address*/ &from,
-            /* wallet_address */ &from, 
+            /* funding address */ &mint_authority.pubkey(),
+            /* wallet_address */ to, 
             /* token_mint_address */ &mint.pubkey(), 
-            /* token_program_id */ &token_program);
+            /* token_program_id */ token_program);
 
         let mint_to_inst = spl_token_interface::instruction::mint_to(
-            &token_program, /* token program */ 
-            &mint.pubkey(), /* mint */
-            &to_ata.0, /* account */
-            &from, /* owner */
-            &[&from], /* signer pubkeys */
-            67 /* amount */
-        ).unwrap();
-
-        svm.airdrop(&from, LAMPORTS_PER_SOL).unwrap();
+            /* token program */ token_program,  
+            /* mint */ &mint.pubkey(), 
+            /* account */ &to_ata.0, 
+            /* owner */ &mint_authority.pubkey(), 
+            /* signer pubkeys */ &[&mint_authority.pubkey()], 
+            /* amount */ mint_amount 
+        )?;
 
         let tx = Transaction::new(
-            &[&from_keypair, &mint], 
+            /* Signers */ &[mint_authority, &mint], 
             Message::new(
                 &[
                     create_mint_account_inst,
@@ -69,21 +80,14 @@ mod litesvm_tests {
                     create_ata_inst,
                     mint_to_inst,
                 ],
-                Some(&from)
+                /* Payer */ Some(&mint_authority.pubkey())
             ), 
             svm.latest_blockhash()
         );
-        let tx_res = svm.send_transaction(tx);
-        match tx_res {
-            Ok(res) => panic!("success: {:#?}", res),
-            Err(f) => panic!("failed: {:#?}", f)
-        }
-
-
-        let from_account = svm.get_account(&from).unwrap();
-        assert_ne!(from_account.lamports, LAMPORTS_PER_SOL);
-
-
+        let _ = svm.send_transaction(tx).unwrap();
+        let to_ata_account = svm.get_account(&to_ata.0).unwrap();
+        let to_ata_token_account = TokenAccount::unpack(&to_ata_account.data).unwrap();
+        assert_eq!(to_ata_token_account.amount, mint_amount);
+        Ok(mint.pubkey())
     }
-
 }
