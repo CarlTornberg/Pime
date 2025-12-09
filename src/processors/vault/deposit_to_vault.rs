c7b3b14ba6623186aae417419bab96d833b71774
Carl Tornberg <carltornberg@hotmail.com>
2025-12-09 07:26:59 +0100

    Added deposit to vault processor structure

---
+++ src/processors/vault/deposit_to_vault.rs
@@ -0,0 +1,8 @@
+use pinocchio::{ProgramResult, account_info::AccountInfo};
+
+pub fn process_deposit_to_vault(accounts: &[AccountInfo], instrution_data: &[u8]) -> ProgramResult {
+    
+    // Check if vault exists
+
+    ProgramResult::Ok(())
+}
