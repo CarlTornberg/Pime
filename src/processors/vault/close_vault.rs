5fbf7d55b87a63ce51090d090fe8050b50eb9c26
Carl Tornberg <carltornberg@hotmail.com>
2025-12-09 07:26:39 +0100

    Added close vault processor structure

---
+++ src/processors/vault/close_vault.rs
@@ -0,0 +1,6 @@
+use pinocchio::{ProgramResult, account_info::AccountInfo};
+
+pub fn process_close_vault(accounts: &[AccountInfo], instrution_data: &[u8]) -> ProgramResult {
+    
+    ProgramResult::Ok(())
+}
