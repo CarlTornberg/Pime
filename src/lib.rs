659c569bfc0bdd74f27745c1e6779e1fb73ef268
Carl Tornberg <carltornberg@hotmail.com>
2025-12-09 07:22:26 +0100

    Init commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,39 @@
 #![no_std]
+
 use pinocchio::{
-  ProgramResult, account_info::AccountInfo, entrypoint, msg, program_error::ProgramError, pubkey::Pubkey
+  ProgramResult, account_info::AccountInfo, default_allocator, hint::unlikely, nostd_panic_handler, program_entrypoint, program_error::ProgramError, pubkey::{Pubkey, pubkey_eq}
 };
 use pinocchio_pubkey::declare_id;
 
-entrypoint!(process_instruction);
-declare_id!("11111111111111111111111111111111");
+pub mod interface;
+mod processors;
+mod shared;
+mod states;
+
+program_entrypoint!(process_instruction);
+default_allocator!();
+nostd_panic_handler!();
+declare_id!("FXvAaHn9TQfDrWZV5X47zYB1r8vcwMPpnDCuTeSafEXw");
 
 pub fn process_instruction(
   program_id: &Pubkey,
   accounts: &[AccountInfo],
   instruction_data: &[u8],
 ) -> ProgramResult {
+    if unlikely(!pubkey_eq(program_id, &ID)) {
+        return Err(ProgramError::IncorrectProgramId);
+    }
 
     let [inst, data @ ..] = instruction_data else {
         return Err(ProgramError::InvalidInstructionData);
     };
 
-    match inst {
-        0 => msg!("This is the first instruction"),
-        _ => msg!("Hello from my program!"),
+    match *inst {
+        0 => {processors::create_vault::process_create_vault(accounts, data)?},
+        _ => {return Err(ProgramError::InvalidInstructionData);}
+        
     }
+
   Ok(())
 }
 
