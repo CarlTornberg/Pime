0702ad3777cdcbaa3faf54264816baa0308a7cdf
Carl Tornberg <carltornberg@hotmail.com>
2025-12-12 14:02:51 +0100

    Added custom error implementation

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,27 @@
-use pinocchio::{program_error::ProgramError, pubkey::Pubkey};
+use pinocchio::program_error::ProgramError;
 
 #[repr(u8)]
 pub enum PimeError {
-    IncorrectPDA(Pubkey, Pubkey) = 0,
+    IncorrectPDA = 0,
+    InvalidTokenProgram = 1,
+    Unserializeable = 2,
+    Undeserializeable = 3,
+
+    Unknown = u8::MAX,
 }
 
+impl From<u8> for PimeError {
+    fn from(value: u8) -> Self {
+        match value { 
+            // SAFETY value is guaranteed to be in range of PimeError enum
+            0..=0 => unsafe { core::mem::transmute::<u8, PimeError>(value) },
+            _ => PimeError::Unknown
+        }
+    }
+}
 
+impl From<PimeError> for ProgramError {
+    fn from(value: PimeError) -> Self {
+        ProgramError::Custom(value as u32)
+    }
+}
