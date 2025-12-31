43dc91ad35aff0730136047d7e7195c0e740a09a
Carl Tornberg <carltornberg@hotmail.com>
2025-12-31 09:35:00 +0100

    Created book transfer instruction data struct

---
+++ src/interface/instructions/book_transfer.rs
@@ -0,0 +1,35 @@
+use pinocchio::sysvars::clock::UnixTimestamp;
+
+use crate::{interface::pime_instruction::PimeInstruction, states::Transmutable};
+
+    ///   - `u64`       The amount to transfer (without decimals).
+    ///   - `u64`       The vault index.
+    ///   - `u64`       The transfer index.
+    ///   - `UnixTimestamp` Warmup period
+    ///   - `UnixTimestamp` Validity period
+#[repr(C)]
+pub struct BookTransferInstructionData {
+    pub discriminator: u8,
+    amount: [u8; size_of::<i64>()],
+    vault_index: [u8; size_of::<u64>()],
+    transfer_index: [u8; size_of::<u64>()],
+    warmup: [u8; size_of::<UnixTimestamp>()],
+    validity: [u8; size_of::<UnixTimestamp>()],
+}
+
+impl BookTransferInstructionData {
+    pub fn new(amount: u64, vault_index: u64, transfer_index: u64, warmup: UnixTimestamp, validity: UnixTimestamp) -> Self{
+        Self { 
+            discriminator: PimeInstruction::BookTransfer as u8, 
+            amount: amount.to_le_bytes(),
+            vault_index: vault_index.to_le_bytes(),
+            transfer_index: transfer_index.to_le_bytes(),
+            warmup: warmup.to_le_bytes(),
+            validity: validity.to_le_bytes(),
+        }
+    }
+}
+
+/// # SAFETY : 
+/// All fields are of u8 and therefore without padding.
+unsafe impl Transmutable for BookTransferInstructionData {}
