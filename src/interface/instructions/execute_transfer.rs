af5c00e1d2f726d7c01f43048d5cc9e27f06daf9
Carl Tornberg <carltornberg@hotmail.com>
2026-01-04 16:35:07 +0100

    Added logic

---
+++ src/interface/instructions/execute_transfer.rs
@@ -0,0 +1,32 @@
+use crate::{interface::pime_instruction::PimeInstruction, states::Transmutable};
+
+#[repr(C)]
+pub struct ExecuteTransferInstructionData {
+    pub discriminator: u8,
+    vault_index: [u8; size_of::<u64>()],
+    transfer_index: [u8; size_of::<u64>()],
+}
+
+/// # SAFETY : 
+/// All fields are of u8 and therefore without padding.
+unsafe impl Transmutable for ExecuteTransferInstructionData {}
+
+impl ExecuteTransferInstructionData{
+    
+    pub fn new(vault_index: u64, transfer_index: u64) -> Self{
+        Self { 
+            discriminator: PimeInstruction::ExecuteTransfer as u8, 
+            vault_index: vault_index.to_le_bytes(),
+            transfer_index: transfer_index.to_le_bytes(),
+        }
+    }
+
+    pub fn vault_index(&self) -> u64 {
+        u64::from_le_bytes(self.vault_index)
+    }
+
+    pub fn transfer_index(&self) -> u64 {
+        u64::from_le_bytes(self.transfer_index)
+    }
+}
+
