3dc0d7637f522a26ba58641a34d06dcb3ba5de72
Carl Tornberg <carltornberg@hotmail.com>
2026-01-15 09:53:00 +0100

    Added close vault inst helper.

---
+++ src/interface/instructions/close_vault_instruction.rs
@@ -0,0 +1,28 @@
+use crate::{interface::pime_instruction::PimeInstruction, states::Transmutable};
+
+#[repr(C)]
+pub struct CloseVaultInstructionData {
+    pub discriminator: u8,
+    vault_index: [u8; size_of::<u64>()],
+}
+
+impl CloseVaultInstructionData {
+    
+    pub fn new(vault_index: u64) -> Self {
+        Self { 
+            discriminator: PimeInstruction::CreateVault as u8, 
+            vault_index: vault_index.to_le_bytes(), 
+        }
+    }
+
+    pub fn vault_index(&self) -> u64 {
+        u64::from_le_bytes(self.vault_index)
+    }
+
+}
+
+/// # SAFETY : 
+/// All fields are of u8 and therefore without padding.
+unsafe impl Transmutable for CloseVaultInstructionData {
+    const LEN: usize = size_of::<Self>();
+}
--- src/interface/instructions/mod.rs
+++ src/interface/instructions/mod.rs
@@ -1,6 +1,7 @@
 pub mod create_vault_instruction;
 pub mod deposit_to_vault_instruction;
 pub mod withdraw_from_vault;
+pub mod close_vault_instruction;
 
 pub mod book_transfer;
 pub mod execute_transfer;
